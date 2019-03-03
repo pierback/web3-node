e559320878127a188bf3e9e78a4fc8a332f13c58
Fabian <fabian.pieringer@student.uni-augsburg.de>
2019-03-03 22:17:21 +0100

    with docker no more unlocking needed

--- utils.js
+++ utils.js
@@ -8,14 +8,6 @@ async function readFileAsync(path) {
   });
 }
 
-async function unlockAccount(web3) {
-  web3.eth.personal.unlockAccount(
-    "0x02e9f84165314bb8c255d8d3303b563b7375eb61",
-    "0000",
-    10000
-  );
-}
-
 module.exports = {
   readFileAsync,
   unlockAccount
