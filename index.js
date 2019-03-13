14594162ed5926ce926d6618c780d3b5bf225f02
Fabian <fabian.pieringer@student.uni-augsburg.de>
2019-03-13 09:47:04 +0100

    ip changes

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const Web3 = require('web3');
 const { startBvgl } = require('./bvrglst');
 const { startCC } = require('./cffcn');
-const { getBchainIp } = require('./utils');
-const { getIP } = require('./udp');
 
 (async function main() {
-  const ip = await getIP();
-  const web3 = await initWeb(ip);
+  // const ip = await getIP();
+  const web3 = await initWeb();
   if (process.argv[2] === 'bl') {
     console.log('process.argv[2]: ', process.argv[2]);
-    startBvgl(web3, ip).catch((err) => {
+    startBvgl(web3, 'oc-appsrv01.informatik.uni-augsburg.de').catch((err) => {
       console.log('Error bvgl: ', err);
     });
   } else if (process.argv[2] === 'cc') {
-    startCC(web3, ip).catch((err) => {
+    startCC(web3, 'oc-appsrv01.informatik.uni-augsburg.de').catch((err) => {
       console.log('Error CC: ', err);
     });
   }
 }());
 
-async function initWeb(ip) {
-  const bchainIp = getBchainIp(ip);
+async function initWeb() {
+  const bchainIp = 'ws://oc-appsrv01.informatik.uni-augsburg.de:8081';// getBchainIp(ip);
   console.log('bchainIp: ', bchainIp);
-  return new Web3(bchainIp);
+  const web3 = new Web3(bchainIp);
+  console.log('web3: ', web3);
+  return web3;
 }
